b1729b616879851a5e74cfe9b0c892a2cc77188b
Ava Moradi <ava.moradi86@gmail.com>
2020-09-21 11:48:50 +0200

    TriviaApp file added

--- Week3/homework/js-exercises/trivia-app/triviaApp.js
+++ Week3/homework/js-exercises/trivia-app/triviaApp.js
@@ -1,49 +1,49 @@
 const containerDiv = document.querySelector(".container");
 const mainDiv = document.querySelector(".main");
 
-
-
+// function fetchAPI
 async function fetchAPI(){
   let response = null;
   let jsonData = null;
   let resaults = null;
-
   try{
     response = await fetch(URL);
     jsonData = await response.json();
-    //console.log(jsonData)
     resaultsArr = jsonData.results;
-    //console.log(resaultsArr)
-    resaultsArr.forEach(element => {
-      mainDiv.innerHTML += `<div class="questionDivStyle">${element.question}</div>`
-    })
-    const objestDiv = document.querySelectorAll(".questionDivStyle");
-    console.log(objestDiv)
   }
-
   catch{
     return new Error('HTTP ERROR!!')
   }
-
+ return resaultsArr;
 }
 
+//function makeDiv
+ async function makeDiv(){
+   console.log(resaultsArr)
+  resaultsArr.forEach(element => {
+   mainDiv.innerHTML += `<div class="questionDivStyle">${element.question}</div>`
+  })
+  const objectDiv = document.querySelectorAll(".questionDivStyle");
+  objectDiv.forEach((element, index) => {
+    let checkFlag = false;
+    element.addEventListener("click", () =>{
+      if (checkFlag === false){
+      correctAnswerDiv = document.createElement('div');
+      correctAnswerDiv.innerText = resaultsArr[index].correct_answer;
+      correctAnswerDiv.className = "correctAnswer";
+      mainDiv.insertBefore(correctAnswerDiv, element.nextSibling);
+      checkFlag = true;
+      }
+    })
+  })
+}
 
-
-
- 
- function showAnswer(arrOfResaults){
-   console.log(arrOfResaults)
-   alarm('hi')
-  //  const answerDiv = document.createElement('div');
-  //  answerDiv.innerText = `${arrOfResaults.correct_answer}`;
-  //  mainDiv.appendChild(answerDiv);
-
- }
-
- // Function main
+// Function main
  async function main(){
   await fetchAPI();
  }
 
  const URL = "https://opentdb.com/api.php?amount=5"
- window.onload = main().then(() => console.log('done'));
+ window.onload = main()
+ .then(async (resaultsArr) =>  await makeDiv(resaultsArr))
+ .catch( (error) => console.log(error))
